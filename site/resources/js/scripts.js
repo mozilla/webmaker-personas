4c7e3071bf11ee911ad47287900a92e1d2dd2870
Ricardo Vazquez <ricardo@mozillafoundation.org>
2014-08-15 14:57:43 -0400

    Adding JS code to fade steps sequentially on scroll

--- site/resources/js/scripts.js
+++ site/resources/js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-  console.log("Ready");
+  console.log("We are Mozilla Webmaker.");
   $('.dropdown-toggle').dropdown();
+
+  $(window).scroll(function() {
+    showSteps();
+  });
+
 });
+
+function showSteps(){
+  $('.step').each(function(i) {
+    $(this).delay((i++) * 650).fadeTo(1000, 1);
+  });
+}
