1148d8bc3c6087daec3cf632f98914b11a4c2d80
Ricardo Vazquez <ricardo@mozillafoundation.org>
2014-08-18 10:00:40 -0400

    Increasing speed of animations

--- site/resources/js/scripts.js
+++ site/resources/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
   $(window).scroll(function () {
     $('.anm-step').each(function (i) {
-      $(this).delay((i++) * 1000).fadeTo(1000, 1);
+      $(this).delay((i++) * 750).fadeTo(750, 1);
     });
   });
 });
